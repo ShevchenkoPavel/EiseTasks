6516580e87c1455b76e9f42b2372d4a3ab8b54c7
ShevchenkoPavel <shevchenkopavel17@gmail.com>
2017-03-21 19:37:21 +0600

    add react-router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './pages/app/App';
 import './index.css';
 
+import createBrowserHistory from 'history/createBrowserHistory';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+	
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <App />,
+  <Router history={history}>
+    <div>
+      <Route exact path='/' component={App}/>
+      <Route path='/app' component={App}/>
+    </div>
+  </Router>,
   document.getElementById('root')
 );
